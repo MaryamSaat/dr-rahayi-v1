36b36bcf2899470cc5278889363d57b7da495899
marysa <maryam2721@yahoo.com>
2025-05-14 14:00:56 +0330

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,17 @@
-fontFamily: {
-    traffic: ['B_Traffic']
-  }
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        traffic: ['"B_Traffic"', "sans-serif"],
+        yekan: ['"BYekan"', "sans-serif"],
+        aleo: ['"Aleo"', "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
